0e10b073df00173f3ccf0254ca2ae94b86eead4a
WillKoehrsen <wjk68@case.edu>
2020-12-24 10:06:57 -0600

    Working on modeling with predictions

--- src/exploration.py
+++ src/exploration.py
@@ -1,153 +1,43 @@
 import os
 import pandas as pd
 
-from src.utils import get_datetime_info
+from src.utils import prepare_data
 
-## CONVERSION FROM EXCEL TO CSV ##
-def read_raw_data():
-    """
-    Read in raw data, either from Excel or csv.
-    """
-    date_read_settings = dict(parse_dates=["date"], index_col="date")
-    date_time_read_settings = dict(parse_dates=["date_time"], index_col="date_time")
 
-    METER_DATA_CSV_NAME = "data/csvs/ConEd_Electric.csv"
+def preliminary():
+    data_dict = prepare_data()
 
-    if not os.path.exists(METER_DATA_CSV_NAME):
-        meter_data = pd.read_excel(
-            "data/raw/ConEd_Electric.xlsx", **date_time_read_settings
-        )
-        meter_data.to_csv(METER_DATA_CSV_NAME)
-    else:
-        print("Meter data found")
-        meter_data = pd.read_csv(METER_DATA_CSV_NAME, **date_time_read_settings)
+    meter_data = data_dict["meter_data"]
+    meter_data.head()
 
-    TENANT_DATA_CSV_NAME = "data/csvs/Tenant_Usage.csv"
-    METERS_CSV_NAME = "data/csvs/Meters.csv"
-    if not os.path.exists(TENANT_DATA_CSV_NAME):
+    tenant_usage_data = data_dict["tenant_data"]
+    tenant_usage_data.head()
 
-        # Create an ExcelFile from the tenant usage.
-        xls = pd.ExcelFile("data/raw/Tenant_Usage.xlsx")
+    tenant_sums = tenant_usage_data.groupby(tenant_usage_data.index)[
+        "consumption", "max_demand", "min_demand", "avg_demand"
+    ].sum()
 
-        sheet_names = xls.sheet_names
-        meter_lookup = pd.read_excel(xls, sheet_names[0])
-        meter_lookup.to_csv(METERS_CSV_NAME, index=False)
+    data_dict["meter_lookup"]
+    occupancy_data = data_dict["occupancy_data"]
 
-        # Read in all the individual tenant consumption data.
-        tenant_data = pd.concat(
-            [
-                pd.read_excel(xls, sheet_name, **date_time_read_settings).assign(
-                    name=sheet_name
-                )
-                for sheet_name in sheet_names[1:]
-            ]
-        )
-        tenant_data.to_csv(TENANT_DATA_CSV_NAME)
-    else:
-        print("Tenant and meter lookup data found")
-        tenant_data = pd.read_csv(TENANT_DATA_CSV_NAME, **date_time_read_settings)
-        meter_lookup = pd.read_csv(METERS_CSV_NAME)
-
-    OCCUPANCY_DATA_CSV_NAME = "data/csvs/Occupancy.csv"
-    if not os.path.exists(OCCUPANCY_DATA_CSV_NAME):
-        occupancy_data = pd.read_excel("data/raw/Occupancy.xlsx", **date_read_settings)
-        occupancy_data.to_csv(OCCUPANCY_DATA_CSV_NAME)
-    else:
-        print("Occupancy data found")
-        occupancy_data = pd.read_csv(OCCUPANCY_DATA_CSV_NAME, **date_read_settings)
-
-    STEAM_DATA_CSV_NAME = "data/csvs/ConEd_Steam.csv"
-    if not os.path.exists(STEAM_DATA_CSV_NAME):
-        steam_data = pd.read_excel(
-            "data/raw/ConEd_Steam.xlsx", **date_time_read_settings
-        )
-        steam_data.to_csv(STEAM_DATA_CSV_NAME)
-    else:
-        print("Steam data found")
-        steam_data = pd.read_csv(STEAM_DATA_CSV_NAME, **date_time_read_settings)
-
-    return dict(
-        meter_data=meter_data,
-        tenant_data=tenant_data,
-        occupancy_data=occupancy_data,
-        steam_data=steam_data,
-        meter_lookup=meter_lookup,
+    daily_meter_sums = meter_data.groupby(meter_data.index.date)["consumption"].sum()
+    daily_totals = occupancy_data.merge(
+        daily_meter_sums, left_index=True, right_index=True
     )
 
-
-def prepare_data():
-    """
-    Read in and create features from data.
-    """
-    data_dict = read_raw_data()
-    meter_data, tenant_data, occupancy_data, steam_data, meter_lookup = (
-        data_dict["meter_data"],
-        data_dict["tenant_data"],
-        data_dict["occupancy_data"],
-        data_dict["steam_data"],
-        data_dict["meter_lookup"],
+    daily_tenant_sums = (
+        tenant_usage_data.groupby(
+            [tenant_usage_data.index.date, tenant_usage_data["meter"]]
+        )["consumption"]
+        .sum()
+        .reset_index()
+        .rename(columns=dict(level_0="date"))
     )
 
-    # Linear interpolation of temperature and humidity values
-    meter_data = interpolate_weather_features(meter_data)
-
-    # Add time and date features
-    meter_data = get_datetime_info(meter_data, date_col="index")
-
-    tenant_data = tenant_data.merge(
-        meter_data.drop(
-            columns=["consumption", "max_demand", "min_demand", "avg_demand"]
-        ),
-        on="date_time",
+    daily_tenant_sums_pivoted = daily_tenant_sums.pivot_table(
+        columns="meter", index="date"
     )
-    return dict(
-        meter_data=meter_data,
-        tenant_data=tenant_data,
-        occupancy_data=occupancy_data,
-        steam_data=steam_data,
-        meter_lookup=meter_lookup,
-    )
+    daily_tenant_sums_pivoted.merge(
+        occupancy_data, left_index=True, right_index=True
+    ).corr()
 
-
-def interpolate_weather_features(dataset):
-    """
-    Linear time-based interpolate of temperature and humidity features.
-    """
-    dataset = dataset.copy()
-    for feature in ["temp", "humidity"]:
-        dataset[feature] = dataset[feature].interpolate(
-            method="time", limit_direction="both"
-        )
-    return dataset
-
-
-
-data_dict = prepare_data()
-
-meter_data = data_dict["meter_data"]
-meter_data.head()
-
-tenant_usage_data = data_dict["tenant_data"]
-tenant_usage_data.head()
-
-tenant_sums = tenant_usage_data.groupby(tenant_usage_data.index)[
-    "consumption", "max_demand", "min_demand", "avg_demand"
-].sum()
-
-data_dict["meter_lookup"]
-occupancy_data = data_dict["occupancy_data"]
-
-daily_meter_sums = meter_data.groupby(meter_data.index.date)["consumption"].sum()
-daily_totals = occupancy_data.merge(daily_meter_sums, left_index=True, right_index=True)
-
-daily_tenant_sums = (
-    tenant_usage_data.groupby(
-        [tenant_usage_data.index.date, tenant_usage_data["meter"]]
-    )["consumption"]
-    .sum()
-    .reset_index()
-    .rename(columns=dict(level_0="date"))
-)
-
-daily_tenant_sums_pivoted = daily_tenant_sums.pivot_table(columns='meter', index='date')
-daily_tenant_sums_pivoted.merge(occupancy_data, left_index=True, right_index=True).corr()
--- src/modeling.py
+++ src/modeling.py
@@ -1,10 +1,26 @@
 import pandas as pd
+import tqdm
+
+import plotly.express as px
+from plotly.offline import plot
 from datetime import date as create_date
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import ExtraTreesRegressor
 
 from src.utils import prepare_data
 
 data_dict = prepare_data()
+
+occupancy_data = data_dict["occupancy_data"]
+
+# First date with greater than 50% reduction in occupancy from baseline (first obs)
+prediction_start_date = occupancy_data.index.date[
+    occupancy_data["baseline_change"] < -50
+][0]
+
+print(
+    f"The first day with greater than 50% reduction in occupancy is {prediction_start_date}."
+)
 
 meter_data = data_dict["meter_data"]
 meter_data.head()
@@ -13,7 +29,11 @@ tenant_usage_data = data_dict["tenant_data"]
 tenant_usage_data.head()
 
 target_cols = ["meter", "consumption", "max_demand", "min_demand", "avg_demand", "name"]
-feature_cols = list(set(tenant_usage_data.columns) - set(target_cols))
+feature_cols = list(
+    set(tenant_usage_data.columns)
+    - set(target_cols)
+    - set(["date", "entries", "baseline_change"])
+)
 features = tenant_usage_data[feature_cols].copy()
 
 targets = tenant_usage_data.pivot_table(
@@ -22,18 +42,10 @@ targets = tenant_usage_data.pivot_table(
 targets.columns = targets.columns.droplevel(0)
 
 meter_cols = targets.columns
-all_feature_coefs = []
+all_feature_coefs_list = []
+all_predictions_list = []
 
-occupancy_data = data_dict["occupancy_data"]
-baseline_entries = occupancy_data["entries"][0]
-occupancy_data["baseline_change"] = (
-    100 * (occupancy_data["entries"] - baseline_entries) / baseline_entries
-)
-prediction_start_date = occupancy_data.index.date[
-    occupancy_data["baseline_change"] < -50
-][0]
-
-for meter in meter_cols:
+for meter in tqdm.tqdm(meter_cols, desc="meters"):
     dataset = (
         targets[[meter]]
         .dropna()
@@ -50,15 +62,46 @@ for meter in meter_cols:
     prediction_df = pd.DataFrame(
         dict(predicted=predictions, actual=prediction_dataset[meter]),
         index=prediction_dataset.index,
-    )
+    ).assign(meter=meter)
     prediction_df["pct_off"] = (
-        prediction_df["actual"] - prediction_df["predicted"]
-    ) / prediction_df["predicted"]
+        100
+        * (prediction_df["actual"] - prediction_df["predicted"])
+        / prediction_df["predicted"]
+    )
 
     feature_coefs = pd.DataFrame.from_dict(
         dict(zip(feature_cols, model.coef_)), orient="index", columns=["coef"]
     ).assign(meter=meter)
 
-    all_feature_coefs.append(feature_coefs)
+    all_feature_coefs_list.append(feature_coefs)
+    all_predictions_list.append(prediction_df)
 
-all_feature_coefs = pd.concat(all_feature_coefs)
+all_feature_coefs = pd.concat(all_feature_coefs_list)
+all_predictions = pd.concat(all_predictions_list)
+all_predictions["date"] = all_predictions.index.date
+
+all_predictions = (
+    all_predictions.reset_index(drop=False)
+    .merge(occupancy_data.reset_index(drop=True), on="date")
+    .set_index("date_time")
+)
+
+
+fig_actual = px.line(
+    all_predictions.reset_index(),
+    x="date_time",
+    y="actual",
+    color="meter",
+    title="Actual Consumption by Meter",
+)
+plot(fig_actual, show_link=True)
+
+fig_predicted = px.line(
+    all_predictions.reset_index(),
+    x="date_time",
+    y="predicted",
+    color="meter",
+    title="Predicted by meter",
+    show_link=True,
+)
+plot(fig_predicted, show_link=True)
