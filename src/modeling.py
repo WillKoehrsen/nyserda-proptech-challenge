874158b7514cb6a103671ddde53d66e94888c756
WillKoehrsen <wjk68@case.edu>
2020-12-26 20:56:20 -0600

    Saved features and targets

--- src/modeling.py
+++ src/modeling.py
@@ -23,11 +23,11 @@ print(
     f"The first day with greater than 50% reduction in occupancy is {prediction_start_date}."
 )
 
-meter_data = data_dict["meter_data"]
-meter_data.head()
-
 tenant_usage_data = data_dict["tenant_data"]
 tenant_usage_data.head()
+
+# Percentage of training data used for validation
+VALIDATION_PERCENTAGE = 0.3
 
 validation_dates = (
     tenant_usage_data.groupby("meter")
@@ -35,12 +35,15 @@ validation_dates = (
         lambda x: x[x["date"] < prediction_start_date]
         .reset_index()["date_time"]
         .astype("int64")
-        .quantile(0.75)
+        .quantile(1 - VALIDATION_PERCENTAGE)
     )
     .astype("datetime64[ns]")
     .dt.date
 ).rename("validation_date")
 
+validation_dates
+
+# Assign training/validation/prediction label to data
 tenant_usage_data.loc[
     tenant_usage_data["date"] >= prediction_start_date, "set"
 ] = "prediction"
@@ -59,137 +62,133 @@ tenant_usage_data.loc[
     tenant_usage_data["date"] < tenant_usage_data["validation_date"], "set"
 ] = "training"
 
+tenant_usage_data.groupby("meter")["set"].value_counts()
 
 target_cols = ["meter", "consumption", "max_demand", "min_demand", "avg_demand", "name"]
 
+# All feature columns
 feature_cols = list(
     set(tenant_usage_data.columns)
     - set(target_cols)
-    - set(["date", "entries", "baseline_change", "validation_date"])
+    - set(["set", "date", "entries", "baseline_change", "validation_date"])
 )
 
+# Important features from random forest model
 important_features = [
-    "date_time_Dayofyear",
-    "date_time_FracDay",
-    "date_time_FracWeek",
-    "date_time_Month",
+    "date_time_FracYear",
+    "date_time_FracMonth",
     "humidity",
+    "date_time_FracDay",
+    "date_time_Trend",
     "temp",
+    "date_time_FracWeek",
 ]
 
-# Get the features with set and meter columns for subsetting
-features = tenant_usage_data[important_features + ["set", "meter"]].copy()
 
-# For the targets, each meter has its own column
-targets = tenant_usage_data.pivot_table(
-    index=["date_time"], values=["consumption"], columns=["meter"]
+def model_with_features(feature_list):
+    """
+    Build a supervised regression model with the specified features.
+    Returns the predictions for each meter and set.
+    """
+    # Get the features with set and meter columns for subsetting
+    features = tenant_usage_data[feature_list + ["set", "meter"]].copy()
+
+    # For the targets, each meter has its own column
+    targets = tenant_usage_data.pivot_table(
+        index=["date_time", "set"], values=["consumption"], columns=["meter"]
+    )
+    targets.columns = targets.columns.droplevel(0)
+
+    targets.head()
+
+    # Each meter forms one column of the targets (consumption values)
+    meter_cols = targets.columns
+
+    all_feature_imps_list = []
+    all_results_list = []
+
+    for meter in tqdm.tqdm(meter_cols, desc="meters"):
+        dataset = (
+            targets[[meter]].dropna()
+            # Merge the targets with their respective features
+            .merge(
+                features[features["meter"] == meter],
+                left_index=True,
+                right_index=True,
+                how="left",
+            )
+        )
+
+        training_dataset = dataset[dataset["set"] == "training"]
+
+        # Train the model on the features and training target
+        model = RandomForestRegressor(n_jobs=-1, max_depth=50, n_estimators=100).fit(
+            X=training_dataset[feature_list], y=training_dataset[meter]
+        )
+
+        validation_dataset = dataset[dataset["set"] == "validation"]
+
+        assert (
+            training_dataset.index.max() + timedelta(minutes=15)
+            <= validation_dataset.index.min()
+        )
+
+        prediction_dataset = dataset[dataset["set"] == "prediction"]
+
+        assert (
+            validation_dataset.index.max() + timedelta(minutes=15)
+            <= prediction_dataset.index.min()
+        )
+
+        validations = model.predict(validation_dataset[feature_list])
+        predictions = model.predict(prediction_dataset[feature_list])
+
+        validation_df = (
+            pd.DataFrame(
+                dict(predicted=validations, actual=validation_dataset[meter]),
+                index=validation_dataset.index,
+            )
+            .assign(meter=meter)
+            .assign(set="validation")
+        )
+        prediction_df = (
+            pd.DataFrame(
+                dict(predicted=predictions, actual=prediction_dataset[meter]),
+                index=prediction_dataset.index,
+            )
+            .assign(meter=meter)
+            .assign(set="prediction")
+        )
+
+        results_df = pd.concat([validation_df, prediction_df])
+
+        feature_imps = pd.DataFrame.from_dict(
+            dict(zip(feature_list, model.feature_importances_)),
+            orient="index",
+            columns=["importance"],
+        ).assign(meter=meter)
+
+        all_feature_imps_list.append(feature_imps)
+        all_results_list.append(results_df)
+
+    all_feature_imps = (
+        pd.concat(all_feature_imps_list)
+        .reset_index()
+        .rename(columns=dict(index="feature"))
+    )
+
+    all_results = pd.concat(all_results_list)
+    all_results["date"] = all_results.index.date
+
+    return dict(all_results=all_results, all_feature_imps=all_feature_imps)
+
+
+results_dict = model_with_features(feature_list=feature_cols[:])
+
+all_results, all_feature_imps = (
+    results_dict["all_results"],
+    results_dict["all_feature_imps"],
 )
-targets.columns = targets.columns.droplevel(0)
-
-meter_cols = targets.columns
-all_feature_imps_list = []
-all_results_list = []
-
-for meter in tqdm.tqdm(meter_cols, desc="meters"):
-    dataset = (
-        targets[[meter]]
-        .dropna()
-        .merge(
-            features[features["meter"] == meter],
-            left_index=True,
-            right_index=True,
-            how="left",
-        )
-    )
-
-    training_dataset = dataset[dataset["set"] == "training"]
-
-    model = RandomForestRegressor(n_jobs=-1, max_depth=30, n_estimators=60).fit(
-        X=training_dataset[important_features], y=training_dataset[meter]
-    )
-
-    validation_dataset = dataset[dataset["set"] == "validation"]
-
-    assert (
-        training_dataset.index.max() + timedelta(minutes=15)
-        == validation_dataset.index.min()
-    )
-
-    prediction_dataset = dataset[dataset["set"] == "prediction"]
-
-    assert (
-        validation_dataset.index.max() + timedelta(minutes=15)
-        == prediction_dataset.index.min()
-    ) or (validation_dataset.index.max() < prediction_dataset.index.min())
-
-    validations = model.predict(validation_dataset[important_features])
-    predictions = model.predict(prediction_dataset[important_features])
-
-    validation_df = (
-        pd.DataFrame(
-            dict(predicted=validations, actual=validation_dataset[meter]),
-            index=validation_dataset.index,
-        )
-        .assign(meter=meter)
-        .assign(set="validation")
-    )
-    prediction_df = (
-        pd.DataFrame(
-            dict(predicted=predictions, actual=prediction_dataset[meter]),
-            index=prediction_dataset.index,
-        )
-        .assign(meter=meter)
-        .assign(set="prediction")
-    )
-
-    results_df = pd.concat([validation_df, prediction_df])
-
-    results_df["pct_off"] = (
-        100 * (results_df["actual"] - results_df["predicted"]) / results_df["actual"]
-    )
-
-    feature_imps = pd.DataFrame.from_dict(
-        dict(zip(important_features, model.feature_importances_)),
-        orient="index",
-        columns=["importance"],
-    ).assign(meter=meter)
-
-    all_feature_imps_list.append(feature_imps)
-    all_results_list.append(results_df)
-
-    validation_mape = (
-        100
-        * (
-            (validation_df["predicted"] - validation_df["actual"])
-            / validation_df["actual"]
-        )
-        .replace({np.inf: pd.NA})
-        .abs()
-        .mean()
-    )
-    prediction_mape = (
-        100
-        * (
-            (prediction_df["predicted"] - prediction_df["actual"])
-            / prediction_df["actual"]
-        )
-        .replace({np.inf: pd.NA})
-        .abs()
-        .mean()
-    )
-
-    print(
-        f"Meter: {meter}. Validation MAPE: {round(validation_mape, 2)}%. Prediction MAPE: {round(prediction_mape, 2)}%"
-    )
-
-all_feature_imps = (
-    pd.concat(all_feature_imps_list).reset_index().rename(columns=dict(index="feature"))
-)
-all_results = pd.concat(all_results_list)
-all_results["date"] = all_results.index.date
-
-all_results["mae"] = all_results["predicted"] - all_results["actual"]
-
 all_results = (
     all_results.reset_index(drop=False)
     .merge(occupancy_data.reset_index(drop=True), how="left", on="date")
@@ -199,10 +198,28 @@ all_results = (
 
 def calculate_corrected_mape(actual, predicted):
     errors = predicted - actual
-    ape = errors / actual
-    ape[(actual == 0) & (predicted == 0)] = 0
-    ape[(actual == 0) & (predicted != 0)] = errors / actual.median()
-    return dict(
-        mpe=round(100 * ape.median(), 2), mape=round(100 * ape.abs().median(), 2)
-    )
+
+    # The percentage error undefined when the actual measurement is 0.
+    pe = errors / actual
+
+    # When both the actual and predicted are 0, clearly the error is 0.
+    pe[(actual == 0) & (predicted == 0)] = 0
+
+    # Where the actual value is 0, but the predicted is not 0, divide by the absolute
+    # mean actual value as an approximation
+    pe[(actual == 0) & (predicted != 0)] = errors / actual.abs().mean()
+
+    # The mean percentage error encodes information about directionality of errors
+    mpe = 100 * pe.mean()
+
+    # The mean absolute percentage error does not count directionality and thus may be
+    # better for comparing model performance.
+    mape = 100 * pe.abs().mean()
+
+    return dict(mpe=round(mpe, 2), mape=round(mape, 2))
+
+
+scores_by_meter = all_results.groupby(["meter", "set"]).apply(
+    lambda x: calculate_corrected_mape(actual=x["actual"], predicted=x["predicted"])
+)
 
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,25 @@
 import os
 import pandas as pd
+import numpy as np
 
 
-def get_datetime_info(df, date_col, timezone=None, drop=False):
+def get_datetime_info(
+    df, date_col, utc=False, timezone=None, drop=False, additional_attributes=False
+):
     """
     Extract date and time information from a column in dataframe
-    and add as new columns. Time zones are converted to local time if specified.
+    and add as new columns. If time is originally is UTC but local timezone is provided,
+    then features will be in local timezone.
 
     :param df: pandas dataframe
     :param date_col: string representing the column containing datetimes. Can also be 'index' to use the index
-    :param timezone: string for the time zone. If passed, times are converted to local
-    :param drop: boolean indicating whether the original column should be dropped from the df
+    :param utc: boolean for whether the timestamps in UTC
+    :param timezone: string for the time zone. If passed, times are converted to local.
+        A local timezone should only be passed if timestamps are originally in utc.
+    :param drop: boolean indicating whether the original column should be dropped from
+        the dataframe
+    :param additional_attributes: boolean for whether additional date attributes should
+        be extracted.
 
     :return df: dataframe with added date and time columns
     """
@@ -25,13 +34,13 @@ def get_datetime_info(df, date_col, timezone=None, drop=False):
         prefix = date_col
 
     # Make sure the field type is a datetime
-    if timezone is not None:
-        fld = pd.to_datetime(fld, utc=True)
-    else:
-        fld = pd.to_datetime(fld)
+    fld = pd.to_datetime(fld, utc=utc)
 
-    # Convert to local time and then remove time zone information
+    # If timezone originally in UTC but want in local time
     if timezone:
+        # Timestamps must originally be in UTC timezone
+        if not utc:
+            raise ValueError("Timestamps were not declared as UTC")
         fld = fld.dt.tz_convert(timezone).dt.tz_localize(None)
         df["local"] = fld
 
@@ -41,37 +50,54 @@ def get_datetime_info(df, date_col, timezone=None, drop=False):
     # Basic attributes
     attr = ["Year", "Month", "Day", "Dayofweek", "Dayofyear", "Date"]
 
-    # Additional attributes to extract
-    attr = attr + [
-        "Is_month_end",
-        "Is_month_start",
-        "Is_quarter_end",
-        "Is_quarter_start",
-        "Is_year_end",
-        "Is_year_start",
-    ]
+    if additional_attributes:
+        attr = attr + [
+            "Is_month_end",
+            "Is_month_start",
+            "Is_quarter_end",
+            "Is_quarter_start",
+            "Is_year_end",
+            "Is_year_start",
+        ]
 
     # Time attributes
     attr = attr + ["Hour", "Minute", "Second"]
 
     # Iterate through each attribute and add it to the dataframe
     for n in attr:
+        # The week or weekofyear attributes are deprecated, so must use
+        # isocalendar().week
         if n == "week":
             df[prefix + n] = getattr(fld.dt.isocalendar(), n.lower())
         else:
             df[prefix + n] = getattr(fld.dt, n.lower())
 
-    # Add fractional time of day
+    # Add fractional time of day by converting to hours
     df[prefix + "FracDay"] = (
         (df[prefix + "Hour"] / 24)
         + (df[prefix + "Minute"] / 60 / 24)
         + (df[prefix + "Second"] / 60 / 60 / 24)
     )
 
-    # Add fractional time of week
+    # Add fractional time of week by converting to hours
     df[prefix + "FracWeek"] = (
         (df[prefix + "Dayofweek"] * 24) + (df[prefix + "FracDay"] * 24)
     ) / (7 * 24)
+
+    # Add fractional time of month by converting to hours
+    df[prefix + "FracMonth"] = (
+        # First day of month is 1
+        ((df[prefix + "Day"] - 1) * 24) + (df[prefix + "FracDay"] * 24)
+    ) / (fld.dt.daysinmonth * 24)
+
+    # Add fractional time of year by converting to hours
+    df[prefix + "FracYear"] = (
+        # First day of year is 1
+        ((df[prefix + "Dayofyear"] - 1) * 24) + (df[prefix + "FracDay"] * 24)
+    ) / (np.where(fld.dt.is_leap_year, 366, 365) * 24)
+
+    # Add a trend which measures days since first measurement
+    df[prefix + "Trend"] = (fld - fld.min()) / pd.Timedelta(days=1)
 
     # Drop the column if specified
     if drop:
@@ -195,6 +221,11 @@ def prepare_data():
         .reset_index()
         .sort_values(by=["meter", "date_time"])
     ).set_index("date_time")
+
+    tenant_data = pd.concat(
+        [tenant_data, meter_data.assign(meter="building")], axis="rows"
+    )
+
     return dict(
         meter_data=meter_data,
         tenant_data=tenant_data,
